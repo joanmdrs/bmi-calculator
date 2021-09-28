b2233addc6c890c46f0aad943e05151aeb1616a7
joanmdrs <joanmedeiros2018@gmail.com>
2021-09-28 20:03:20 -0300

    Criação de um app simples para cálculo do IMC, utilizando React Native

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Title from './src/components/Title/index.js'
+import Form from './src/components/Form/index.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title />
+      <Form />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: '#E6E6E6',
+    paddingTop: 80, 
+    
+  }
+})
